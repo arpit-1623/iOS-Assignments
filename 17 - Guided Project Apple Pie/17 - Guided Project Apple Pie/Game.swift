e9d8cc56057947aab304968e5b3e0b4176eaf504
Arpit Garg <arpitgarg@Arpits-MacBook-Air.local>
2025-07-19 09:29:23 +0530

    19th July, 2025

---
+++ 17 - Guided Project Apple Pie/17 - Guided Project Apple Pie/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  17 - Guided Project Apple Pie
+//
+//  Created by Arpit Garg on 18/07/25.
+//
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
