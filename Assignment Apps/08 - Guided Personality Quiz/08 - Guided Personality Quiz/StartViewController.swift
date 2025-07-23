767383518221f62c80190bcea687bccd224dd128
Arpit Garg <arpitgarg@Arpits-MacBook-Air.local>
2025-07-23 18:19:22 +0530

    23rd July, 2025

---
+++ Assignment Apps/08 - Guided Personality Quiz/08 - Guided Personality Quiz/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  08 - Guided Personality Quiz
+//
+//  Created by Arpit Garg on 23/07/25.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
