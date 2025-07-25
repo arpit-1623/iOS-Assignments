1a62765e464037b125b99253b2fc41f85fdd3221
Arpit Garg <arpitgarg@Arpits-MacBook-Air.local>
2025-07-25 09:11:21 +0530

    Guided Personality Quiz

--- Assignment Apps/08 - Guided Personality Quiz/08 - Guided Personality Quiz/ResultsViewController.swift
+++ Assignment Apps/08 - Guided Personality Quiz/08 - Guided Personality Quiz/ResultsViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var responses: [Answer]
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        let frequency = responses.reduce(into: [AnimalType: Int]()) {
+            (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+        
+        let frequentAnswerSorted = frequency.sorted(by:
+                                                        { (pair1, pair2) in return pair1.value > pair2.value })
+        
+        let mostCommonAnswer = frequentAnswerSorted.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
 }
--- Assignment Apps/08 - Guided Personality Quiz/08 - Guided Personality Quiz/StartViewController.swift
+++ Assignment Apps/08 - Guided Personality Quiz/08 - Guided Personality Quiz/StartViewController.swift
@@ -14,6 +14,10 @@ class StartViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        // This method is intentionally left empty. It allows the app to unwind to this view controller.
+    }
+
 
 }
 
