9f295ad07e0142c4e1fe7b51460076ec213d7493
Arpit Garg <arpitgarg@Arpits-MacBook-Air.local>
2025-08-03 21:28:40 +0530

    ST1 Revision

---
+++ ST1 Practice/ST1_Practice/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ST1_Practice
+//
+//  Created by Arpit Garg on 02/08/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindToMainViewController(_ unwindSegue: UIStoryboardSegue) {
+    }
+
+
+}
+
